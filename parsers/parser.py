7294f78879c5fbf89d13b7d0c03fc4f7a790887a
AbdulazizMansurov <116558612+AbdulazizMansurov@users.noreply.github.com>
2022-10-26 13:05:04 +0500

    Update parser.py

--- parsers/parser.py
+++ parsers/parser.py
@@ -6,14 +6,11 @@ class Marvel_News():
         req = requests.get("https://pluggedin.ru/keywords/Marvel").text
         soup = BeautifulSoup(req, "html.parser")
         last_news = soup.find("li", style="margin-bottom: 15px")
-        # print(last_news)
-        link_1 = str(last_news.find("a", href=str)).split(" ")
-        name = (last_news.find("a", href=str)).get_text()
-        link = link_1[2].split("=")[1].split(">")[0].split("/")
-        linker = f"https://pluggedin.ru/keywords/Marvel/open{link[1]}/{link[2]}"
+        name = last_news.find("a", class_="search_title").get_text()
+        link = "pluggedin.ru" + last_news.find("a").get("href")
         new = {
             "name": name,
-            "link": linker,
+            "link": link,
             "set": "pluggedin.ru"
         }
         return new
@@ -23,10 +20,27 @@ class Anime_parser():
         req = requests.get("https://kg-portal.ru/news/anime/").text
         soup = BeautifulSoup(req, "html.parser")
         all = soup.find("div", class_="news_box anime_cat")
-        all_too = all.find_all("a")
-        text = all_too[2].get_text()
+        all_too = all.find_all("a")[1]
+        text = all_too.get_text()
+        link = "kg-portal.ru" + (all_too.get("href"))
         new = {
             "name": text,
+            "link": link,
+            "set": "kg-portal.ru"
+        }
+        return new
+
+class Genshin_parser():
+    def show_genshin_new(self):
+        req = requests.get("https://www.goha.ru/games/genshin-impact/news").text
+        soup = BeautifulSoup(req, "html.parser")
+        all = soup.find("div", class_="component-article-tile")
+        text = all.find("div", class_="shortly").get_text()
+        link = all.find("div", class_="title")
+        link = link.find("a").get("href")
+        new = {
+            "name": text,
+            "link": link,
             "set": "kg-portal.ru"
         }
         return new
