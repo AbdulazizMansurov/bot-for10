2ec034cdde388c58364cda397b6768e06c64f355
AbdulazizMansurov <116558612+AbdulazizMansurov@users.noreply.github.com>
2022-10-26 13:03:48 +0500

    Update text_handlers.py

--- handlers/users/text_handlers.py
+++ handlers/users/text_handlers.py
@@ -15,7 +15,8 @@ def give_marvel_n(message: Message):
         while True:
             data = Marvel_News().show_marvel_new()
             if text != data["name"]:
-                mess = f"""Новость!\n{data["name"]}
+                mess = f"""<b>Новость!<b>\n{data["name"]}
+<a href="{data["link"]}">Подробная информация</a>"
 Данные взяты с сайта {data["set"]}\n
 Спасибо что воспользовались нашим ботом!
 <a href="https://t.me/news_for_10_abdu_bot">Бот Абдулазиза</a>
@@ -23,19 +24,37 @@ def give_marvel_n(message: Message):
                 bot.send_message(chat_id, mess, reply_markup=return_to_main_menu())
                 text = data["name"]
                 time.sleep(60)
+
     elif message.text == "Anime":
         bot.send_message(chat_id, "Новость подбирается...", reply_markup=ReplyKeyboardRemove())
         while True:
             data = Anime_parser().show_anime_new()
             if text != data["name"]:
                 mess = f"""<b>Новость!</b>\n
-    {data["name"]}
-    Данные взяты с сайта {data["set"]}\n
-    Спасибо что воспользовались нашим ботом!
-    <a href="https://t.me/news_for_10_abdu_bot">Бот Абдулазиза</a>"""
+{data["name"]}
+<a href="{data["link"]}">Подробная информация</a>"
+Данные взяты с сайта {data["set"]}\n
+Спасибо что воспользовались нашим ботом!
+<a href="https://t.me/news_for_10_abdu_bot">Бот Абдулазиза</a>"""
                 bot.send_message(chat_id, mess, reply_markup=return_to_main_menu())
                 text = data["name"]
                 time.sleep(60)
+
+    elif message.text == "Genshin impact":
+        bot.send_message(chat_id, "Новость подбирается...", reply_markup=ReplyKeyboardRemove())
+        while True:
+            data = Genshin_parser().show_genshin_new()
+            if text != data["name"]:
+                mess = f"""<b>Новость!</b>\n
+{data["name"]}
+<a href="{data["link"]}">Подробная информация</a>"
+Данные взяты с сайта {data["set"]}\n
+Спасибо что воспользовались нашим ботом!
+<a href="https://t.me/news_for_10_abdu_bot">Бот Абдулазиза</a>"""
+                bot.send_message(chat_id, mess, reply_markup=return_to_main_menu())
+                text = data["name"]
+                time.sleep(60)
+
 
 @bot.callback_query_handler(func=lambda call: call.data=="rtr_tm")
 def ret_to_main_menu(call: CallbackQuery):
