fb272dce0b8e8474bb46c8842a08ba8a456e64c9
AbdulazizMansurov <116558612+AbdulazizMansurov@users.noreply.github.com>
2022-10-27 15:02:37 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,5 @@ from keyboards import show_main_menu
 @bot.message_handler(commands=["start"])
 def start(message: Message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "Добро пожаловать в бота.\nВыберите категорию", reply_markup=show_main_menu())
+    if chat_id in permitted_users:
+        bot.send_message(chat_id, "Добро пожаловать в бота.\nВыберите категорию", reply_markup=show_main_menu())
